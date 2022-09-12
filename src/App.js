64cc20529da557cd0c2b0b8a8fa0aa87f9b8f4f1
emericgautier <emericgtr@gmail.com>
2022-09-12 15:02:02 +0200

    Create navigation pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import LikePage from "./pages/LikePage";
 
 const App = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coups-de-coeur" element={<LikePage />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
